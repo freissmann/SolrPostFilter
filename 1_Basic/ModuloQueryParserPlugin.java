9791d5f7e76715ed6557a964733bcfb34163b94c
freissmann <f_reissmann@web.de>
2014-11-13 14:50:45 +0100

    Create ModuloQueryParserPlugin.java

---
+++ 1_Basic/ModuloQueryParserPlugin.java
@@ -0,0 +1,30 @@
+package de.qaware.blog.solr;
+
+import org.apache.lucene.search.Query;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.search.QParser;
+import org.apache.solr.search.QParserPlugin;
+import org.apache.solr.search.SyntaxError;
+
+public class ModuloQueryParserPlugin extends QParserPlugin {
+
+    @Override
+    public QParser createParser(String qstr, SolrParams localParams,
+            SolrParams params, SolrQueryRequest req) {
+
+        return new QParser(qstr, localParams, params, req) {
+
+            @Override
+            public Query parse() throws SyntaxError {
+                return new ModuloQuery();
+            }
+        };
+    }
+
+    @Override
+    public void init(NamedList args) {
+        // Emtpy on purpose
+    }
+}
